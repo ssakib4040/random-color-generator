9ae7fbe50df6e153540ed4d431390516b1e0e864
Sadman Sakib <101496701+ssakib4040@users.noreply.github.com>
2022-07-16 01:35:14 +0600

    copy button added

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const btn = document.getElementById("btn");
+const copyBtn = document.getElementById("copyBtn");
+const color = document.querySelector(".color");
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+randomBodyColor();
 
 btn.addEventListener("click", function () {
   randomBodyColor();
 });
-randomBodyColor();
+
+copyBtn.addEventListener("click", () => {
+  const colorCode = document.querySelector(".color").textContent;
+  copyBtn.innerText = "Copied!";
+
+  navigator.clipboard.writeText(colorCode);
+  setTimeout(() => {
+    copyBtn.innerText = "Copy";
+  }, 3000);
+});
 
 function randomBodyColor() {
   document.body.style.backgroundColor = `rgba(${random(0, 255)}, ${random(
